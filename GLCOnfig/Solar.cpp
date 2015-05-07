ab82bdc4e087d9290fb572d23fecfd8f28f3a065
N Puneeth <puneeth97@gmail.com>
2015-05-07 16:32:50 +0530

    Solar

---
+++ GLCOnfig/Solar.cpp
@@ -0,0 +1,531 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#else
+#include <GL/glut.h>
+#endif
+#ifdef _WIN32 
+#define _CRT_SECURE_NO_DEPRECATE 
+#define _CRT_SECURE_NO_DEPRECATE 
+#endif 
+#include <stdio.h> 
+#include <iostream>
+
+
+using namespace std;
+
+GLfloat xRot, yRot, zRot, x = -1.0, y, z;
+
+GLfloat SpeedX = 0.25;
+GLfloat year = 0.0; //degrees
+GLfloat day = 0.0;
+GLfloat EarthOrbitRadius = 1.0;
+GLfloat MoonItsSelf = 0.0;
+GLfloat MoonOrbitRadius = 0.1;
+GLfloat MoonAroundEarthRate = 2 * SpeedX;
+GLfloat MoonRotationItselfRate = 5.0 * SpeedX;
+GLfloat daySpeed = 2.0 * SpeedX;
+GLfloat DaysPerYear = 360;
+GLfloat yearSpeed = DaysPerYear / 360.0 * daySpeed * SpeedX;
+
+GLUquadric *quad;
+static GLuint texName;
+
+
+#define pi 3.14159265358979323846
+
+#define SSize 0.2*1.5
+#define MSize 0.016*1.5
+#define VSize 0.03*1.5
+#define ESize 0.035*1.5
+#define MarsSize 0.037*1.5
+#define JSize 0.09*1.5
+#define NeptuneSize 0.04
+
+
+
+#define stripeWidth 64
+#define stripeHeight 64
+static GLubyte stripeImage[stripeHeight][stripeWidth][4];
+
+
+
+void createImage(void)
+{
+	int i, j, c;
+
+	for (i = 0; i < stripeHeight; i++) {
+		for (j = 0; j < stripeWidth; j++) {
+			// Use bit operations to set values to 0 or 255 in stripe pattern
+			c = ((((i & 0x6) == 1) ^ ((j & 0x6)) == 0)) * 255;
+			stripeImage[i][j][0] = (GLubyte)c;
+			stripeImage[i][j][1] = (GLubyte)c;
+			stripeImage[i][j][2] = (GLubyte)125;
+			stripeImage[i][j][3] = (GLubyte)125;
+		}
+	}
+}
+
+typedef struct Planet
+{
+	GLfloat DaysPerYear, OrbitR, Size;
+	GLfloat Clx, Cly, Clz;
+	GLfloat yearSpeed, day, year;
+
+} Planet;
+
+Planet Mercury = { 88, 0.3, MSize,
+0.8, 0.8, 0.0 
+, 0.0, 0.0, 0.0 };
+Planet Venus = { 224, 0.4, VSize,
+0.0, 0.0, 0.0 
+, 0.0, 0.0, 0.0 };
+Planet Earth = { 365, 0.7, ESize,
+0.0, 1.0, 0.0
+, 0.0, 0.0, 0.0 };
+Planet Mars = { 686, 0.9, MarsSize,
+0.8, 0.2, 0.2,  
+0.0, 0.0, 0.0 };
+Planet Jupiter = { 3960, 1.3, JSize,
+1.0, 0.0, 0.0,
+0.0, 0.0, 0.0
+};
+Planet Neptune = { 30000, 2.3, NeptuneSize,
+0.2, 0.2, 0.9,   //color 
+0.0, 0.0, 0.0 };
+
+
+void SpeedGen()
+{
+	Mercury.yearSpeed = Mercury.DaysPerYear / 360.0 * daySpeed*12.0 * SpeedX;
+	Venus.yearSpeed = Venus.DaysPerYear / 360.0 * daySpeed *2.0* SpeedX;
+	Earth.yearSpeed = Earth.DaysPerYear / 360.0 * daySpeed * SpeedX;
+	Mars.yearSpeed = Mars.DaysPerYear / 360.0 * daySpeed *0.5* SpeedX;
+	Jupiter.yearSpeed = Mars.DaysPerYear / 360.0 * daySpeed *0.4* SpeedX;
+	Neptune.yearSpeed = Neptune.DaysPerYear / 360.0 * daySpeed *0.009* SpeedX;
+
+}
+
+
+void PlacePlanet(Planet x)
+{
+	glRotatef(x.year, 0.2, 1.0, 0.1);
+	glTranslatef(x.OrbitR, 0.0, 0.0);
+	glRotatef(-(x.year), 0.2, 1.0, 0.1);
+	glPushMatrix();
+	glRotatef(x.day, 0.25, 1.0, 0.0);
+	glColor3f(x.Clx, x.Cly, x.Clz);
+	//glutSolidSphere(x.Size, 20, 20);  
+	//glutSolidSphere(x.Size,50,50);
+	glutWireSphere(x.Size, 50, 50);
+	glBegin(GL_LINES);
+		glVertex3f(x.OrbitR,0 , 0);
+
+		glVertex3f(x.OrbitR+0.1, 0, 0);
+	
+	glEnd();
+	
+	glPopMatrix();
+}
+
+GLubyte fish[] = {
+	0x00, 0x60, 0x01, 0x00,
+	0x00, 0x90, 0x01, 0x00,
+	0x03, 0xf8, 0x02, 0x80,
+	0x1c, 0x37, 0xe4, 0x40,
+	0x20, 0x40, 0x90, 0x40,
+	0xc0, 0x40, 0x78, 0x80,
+	0x41, 0x37, 0x84, 0x80,
+	0x1c, 0x1a, 0x04, 0x80,
+	0x03, 0xe2, 0x02, 0x40,
+	0x00, 0x11, 0x01, 0x40,
+	0x00, 0x0f, 0x00, 0xe0,
+};
+
+GLfloat randomFloat() {
+	return (GLfloat)rand() / RAND_MAX;
+}
+
+
+
+void Display()
+{	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Model view matrix mode
+	
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glMatrixMode(GL_MODELVIEW);
+	// Reset the model-view matrix
+	// This is done each time you need to clear the matrix operations
+	// This is similar to glPopMatrix()
+	
+
+	//RenderScene();
+	glPushMatrix();
+	gluLookAt(0.0, 0.0, -3.0,
+		0.0, 0.0, 5.0,
+		0.0, -4.0, 0.0);
+
+	glPushMatrix();
+
+
+
+	GLfloat black[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat yellow[] = { 1.0, 1.0, 0.0, 1.0 };
+	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat direction[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat direction2[] = { 1.0, 1.0, 0.0, 1.0 };
+	GLfloat sunny[] = { 0.92f, 0.53f, 0.18f,0.0};
+	GLfloat red[] = { 1.0, 0, 0, 0 };
+	float difLight0[4] = { 0.5f, 0.5f, 0.5f, 1.0f };
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, yellow);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, white);
+	glMaterialf(GL_FRONT, GL_SHININESS, 15);
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, sunny);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, difLight0);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+	glLightfv(GL_LIGHT0, GL_POSITION, direction);
+
+	glNormal3f(0, 0, 0);
+	glColor3f(0.92f, 0.53f, 0.18f);
+	
+	quad = gluNewQuadric();
+	gluQuadricTexture(quad, 1);
+
+	gluSphere(quad, 0.25, 20, 20);
+
+	//glutSolidSphere(SSize, 150, 250);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+
+	glPopMatrix();
+
+
+	glPushMatrix();
+
+	glPushMatrix();
+	//glDisable(GL_LIGHTING);
+	//glDisable(GL_LIGHT0);
+	//glDisable(GL_DEPTH_TEST);
+
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Mercury);
+	glPopMatrix();
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Venus);
+	glPopMatrix();
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Earth);
+	glPopMatrix();
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Mars);
+	glPopMatrix();
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Jupiter);
+	glPopMatrix();
+
+	glPushMatrix();
+	glEnable(GL_COLOR_MATERIAL);
+	PlacePlanet(Neptune);
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glLoadIdentity();
+	// Add a Cyan wire teapot
+	glColor3f(0.6f, 0.7f, 0.85f);
+	glLoadIdentity();
+	// Translate to center
+	glTranslatef(-1.8f, 1.5f, -6.0f);
+	glScalef(0.3f, 0.3f, 0.3f);
+	// Rotate 45 degree steps across z,y and x.
+	glRotatef(45.0f, 0, 0, 1.0);
+	//glRotatef(45.0f, 0, 1.0, 0.0);
+	glRotatef(45.0f, 1.0, 0.0, 0.0);
+	glutSolidTeapot(0.5);
+
+	glPushMatrix();
+
+	// Reset Matrix and then translate and scale a Cube
+	glLoadIdentity();
+	glTranslatef(2.5f, 1.5f, -6.0f);
+	glScalef(0.25f, 0.25f, 0.25f);
+	glRotatef(45.0f, 1.0f, 0.0f, 0.0f);
+
+
+	// Cube - 6 sides different colors
+	glBegin(GL_QUADS);
+	// Top Green 
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glTexCoord3f(0.0, 0.0,0.0); glVertex3f(1.0f, 1.0f, -1.0f);
+	glTexCoord3f(0.0, 1.0, 0.0); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glTexCoord3f(1.0, 1.0, 0.0); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord3f(1.0, 1.0, 1.0); glVertex3f(1.0f, 1.0f, 1.0f);
+
+	// Bottom Orange
+	glColor3f(1.0f, 0.5f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	// Front Red
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glTexCoord3f(1.0, 1.0, 1.0); glVertex3f(1.0f, 1.0f, 1.0f);
+	glTexCoord3f(0.0, 1.0, 0.0); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord3f(0.0, 0.0, 0.0); glVertex3f(-1.0f, -1.0f, 1.0f);
+	glTexCoord3f(1.0, 0.0, 1.0); glVertex3f(1.0f, -1.0f, 1.0f);
+
+	//Back Yellow
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	//Left Blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glTexCoord3f(1.0, 1.0, 1.0); glVertex3f(-1.0f, 1.0f, 1.0f);
+	glTexCoord3f(0.0, 1.0, 1.0); glVertex3f(-1.0f, 1.0f, -1.0f);
+	glTexCoord3f(1.0, 0.0, 1.0); glVertex3f(-1.0f, -1.0f, -1.0f);
+	glTexCoord3f(0.0, 0.0, 0.0); glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	// Right Cyan
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glLoadIdentity();
+	glColor3f(0.75f, 0.75f, 0.75f);
+	glTranslatef(2.5f, 1.5f, -6.0f);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.95f, -0.1f, 0.0);
+	glVertex3f(-0.75f, 0.2f, 0);
+	glVertex3f(-0.25f, 0.2f, 0);
+	glVertex3f(-0.45f, -0.1f, 0);
+	glEnd();
+
+	glLoadIdentity();
+	glTranslatef(3.8f, 1.5f, -6.0f);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.95f, -0.1f, 0.0);
+	glVertex3f(-0.75f, 0.2f, 0);
+	glVertex3f(-0.25f, 0.2f, 0);
+	glVertex3f(-0.45f, -0.1f, 0);
+	glEnd();
+
+
+	glPopMatrix();
+
+	glPushMatrix();
+	//glLoadIdentity();
+
+	for (int i = 0; i < 20; i++) {
+		glColor3f(randomFloat(), randomFloat(), randomFloat());
+		glRasterPos3f(randomFloat(), randomFloat(), 0.0);
+		glBitmap(27, 15, -100, 0, 5, 5, fish);
+	}
+	glPopMatrix();
+
+	glPopMatrix();
+	glPopMatrix();
+
+
+	glPopMatrix();
+
+	glFlush();
+	glutSwapBuffers();
+
+}
+
+void winReshapeFcn(GLsizei width, GLsizei height) {
+	// Compute aspect ratio of the new window
+	if (height == 0)
+		height = 1;
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+	// Set the viewport 
+	// Allows for resizing without major display issues
+	glViewport(0, 0, width, height);
+
+	// Set the aspect ratio of the clipping volume to match the viewport
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Enable perspective projection with fovy, aspect, zNear and zFar
+	// This is the camera view and objects align with view frustrum 
+	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+
+void Idle()
+{
+
+	
+	Mercury.day += daySpeed;
+	Mercury.year += Mercury.yearSpeed;
+
+	Venus.day += daySpeed;
+	Venus.year += Venus.yearSpeed;
+
+	Mars.day += daySpeed;
+	Mars.year += Mars.yearSpeed;
+
+	Earth.day += daySpeed;
+	Earth.year += Earth.yearSpeed;
+
+	Jupiter.day += daySpeed;
+	Jupiter.year += Jupiter.yearSpeed;
+
+	Neptune.day += daySpeed;
+	Neptune.year += Neptune.yearSpeed;
+
+	Display();
+
+}
+void Init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	// Get and display your OpenGL version
+	const GLubyte *Vstr;
+	Vstr = glGetString(GL_VERSION);
+	//fprintf(stderr, "Your OpenGL version is %s\n", Vstr);
+	
+	glClearDepth(50.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	createImage();
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Bind the texture
+	glGenTextures(1, &texName);
+	glBindTexture(GL_TEXTURE_2D, texName);
+
+	// Set the repeat wrap flags and filter to adjust images 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+		GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+		GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, stripeWidth,
+		stripeHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+		stripeImage);
+
+}
+
+void mouseFcn(GLint button, GLint action, GLint x, GLint y)
+{
+
+	switch (button) {
+	case GLUT_LEFT_BUTTON:           //  Start the animation.
+		if (action == GLUT_DOWN)
+			glutIdleFunc(Idle);
+		break;
+	case GLUT_RIGHT_BUTTON:            //  Stop the animation.
+		if (action == GLUT_DOWN)
+			glutIdleFunc(NULL);
+		break;
+	default:
+		break;
+	}
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case '1':   /*  p key starts the animation  */
+		glutIdleFunc(Idle);
+		break;
+	case '2': /*  2 key stops the animation  */
+		glutIdleFunc(NULL);
+		break;
+	case 'q': 		
+		daySpeed = 0; 
+		break;
+	case 'w': 
+		daySpeed = 2.0 * SpeedX; 
+		break;
+	case 'j': 
+		Jupiter.yearSpeed = 0;
+		break;
+	case 'm': 
+		Mercury.yearSpeed = 0;
+		break;
+	case 'n': 
+		Neptune.yearSpeed = 0;
+		break;
+	case 'b': 
+		Mars.yearSpeed = 0;
+		break;
+	case 'e':
+		Earth.yearSpeed = 0;
+		break;
+	case 'v': 
+		Earth.yearSpeed = 0;
+		break;
+	case 'r': 
+		SpeedGen();
+		break;
+	default:
+		break;
+	}
+	}
+
+
+
+void main(int argc, char **argv)
+{
+	// Initialize GLUT
+	printf("Instructions:\n");
+	printf("To Start the Animation:\t Press 1 on keyboard or Left Click \n");
+	printf("To Start the Animation:\t Press 2 on keyboard or Right Click \n");
+	printf("To Stop the Rotation:\t Press q Restart:\t Press w\n");
+	printf("To Stop Jupiter's revolution: \t Press j \n");
+	printf("To Stop Mars's revolution: \t Press b\n");
+	printf("To Stop Earth's revolution: \t Press e \n");
+	printf("To Stop Venus's revolution: \t Press v\n");
+	printf("To Stop Neptune's revolution: \t Press n\n");
+	printf("To Stop Mercury's revolution: \t Press m \n");
+	printf("To Restart Any planet's revolution: \t Press r \n");
+	SpeedGen();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // double buffering for Animation
+	glutInitWindowSize(1000, 700);
+	glutInitWindowPosition(800, 100);
+
+	glutCreateWindow("Solar System");
+	//Functions
+	Init();
+	//
+	glutDisplayFunc(Display);
+	glutReshapeFunc(winReshapeFcn);
+	//glutIdleFunc(Idle);
+	glutKeyboardFunc(keyboard);
+	//glutTimerFunc(10, timer, 0);
+	glutMouseFunc(mouseFcn);
+
+	
+	glutMainLoop();
+
+}
